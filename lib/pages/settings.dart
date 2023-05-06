879f15331ab6404b5f4ffba6777051cde3a604c9
Rynxiao <yuzhongzi91@sina.com>
2023-05-06 16:55:34 +0800

    feat: add simple settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/custom_scaffold.dart';
 import '../routes/routes.dart';
+import '../states/global_state.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final globalState = Provider.of<GlobalState>(context);
+    final isDarkMode = globalState.isDarkMode;
+
     return CustomScaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text("Settings")
-          ],
+        child: Container(
+          color: Theme.of(context).colorScheme.background,
+          child: ListView(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.sunny),
+                title: Text(isDarkMode ? '暗黑模式' : '明亮模式'),
+                trailing: Switch(
+                  value: globalState.isDarkMode,
+                  onChanged: (bool value) {
+                    globalState.toggleTheme();
+                  },
+                ),
+                onTap: () {
+                  // TODO: navigate to profile page
+                },
+              ),
+              const Divider(),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: renderBottomNavigationBar(context, 1),
